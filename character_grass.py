a9f370d328cb3bb3aad5df6720be7c5959cdd68c
yeonsu <yeonsu9205@naver.com>
2024-09-24 10:01:54 +0900

    캐릭터출력

--- character_grass.py
+++ character_grass.py
@@ -1,18 +1,21 @@
 from pico2d import *
 
+open_canvas()
+
+# fill here
+grass = load_image('grass.png')
+boy = load_image('character.png')
+
 def run_rectangle():
     print('RECTANGLE')
     pass
 
 def run_circle():
     print('CIRCLE')
-    pass
 
-open_canvas()
-
-# fill here
-grass = load_image('grass.png')
-boy = load_image('character.png')
+    clear_canvas_now()
+    boy.draw_now(400,300)
+    delay(1)
 
 while True:
     run_circle()
