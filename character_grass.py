729b560dd89f584f299064afd4d4098bf0f96889
yeonsu <yeonsu9205@naver.com>
2024-09-24 09:49:34 +0900

    전체적인 구조 완성
    사각형 운동, 원 운동의 무한 루프 구조 완성

---
+++ character_grass.py
@@ -0,0 +1,21 @@
+from pico2d import *
+
+def run_rectangle():
+    pass
+
+def run_circle():
+    pass
+
+
+open_canvas()
+
+# fill here
+grass = load_image('grass.png')
+boy = load_image('character.png')
+
+while True:
+    run_rectangle()
+    run_circle
+
+close_canvas()
+    
